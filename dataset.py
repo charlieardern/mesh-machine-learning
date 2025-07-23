338acd4541d1ec6ed7e6a957c983b7235fd83133
charlieardern <charlie.ardern@gmail.com>
2025-07-23 15:24:35 +0100

    Removed unneeded y normalisation in dataset

--- dataset.py
+++ dataset.py
@@ -8,7 +8,6 @@ class ModelNetSubset(Dataset):
     def __init__(
         self,
         train: bool,
-        norm_params_y: tuple[torch.Tensor, torch.Tensor] | None,
         norm_params_x: tuple[torch.Tensor, torch.Tensor] | None,
     ) -> None:
         # Apply normalisation to x:
@@ -28,15 +27,7 @@ class ModelNetSubset(Dataset):
             self.norm_params_x = norm_params_x
             self.x = (x - norm_params_x[0]) / norm_params_x[1]
 
-        # Apply normalisation to y:
-        if norm_params_y is None:
-            mean_y = y.mean(dim=0)
-            std_y = y.std(dim=0)
-            self.norm_params_y = mean_y, std_y
-            self.y = (y - mean_y) / std_y
-        else:
-            self.norm_params_y = norm_params_y
-            self.y = (y - norm_params_y[0]) / norm_params_y[1]
+        self.y = y
 
     def __len__(self) -> int:
         return len(self.x)
@@ -47,9 +38,8 @@ class ModelNetSubset(Dataset):
         return x_i, y_i
 
 
-train_dataset = ModelNetSubset(train=True, norm_params_x=None, norm_params_y=None)
+train_dataset = ModelNetSubset(train=True, norm_params_x=None)
 test_dataset = ModelNetSubset(
     train=False,
     norm_params_x=train_dataset.norm_params_x,
-    norm_params_y=train_dataset.norm_params_y,
 )
