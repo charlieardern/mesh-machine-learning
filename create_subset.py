95dfdc740f2acd45406ac73f4b110dbd110d9e37
charlieardern <charlie.ardern@gmail.com>
2025-07-24 15:37:56 +0100

    Removed commented debugging code from create_subset

--- create_subset.py
+++ create_subset.py
@@ -18,13 +18,11 @@ train_y = []
 
 # Create training set:
 for i in range(len(train_set)):
-    # print(f"Processing object {i + 1}...")
     data_y = train_set[i].y
     data_x_full = train_set[i].pos
     d = torch.tensor([1000.0])
     c = 0  # this counts number of successive rejections
     N = data_x_full.shape[0]
-    # print(f"N: {N}")
     samples = data_x_full[torch.randint(0, N, (1,))]  # create first data point
 
     while samples.shape[0] < new_size:
@@ -32,21 +30,15 @@ for i in range(len(train_set)):
         sample_i = data_x_full[idx]
         distances = torch.norm(samples - sample_i, dim=1)
         within_dist = (distances + 0.01 <= d).any()
-        # print(f"d: {d}, distances: {distances}")
         if not within_dist:
-            # print(f"samples shape: {samples.shape}")
-            # print(f"sample_i shape: {sample_i.shape}")
             samples = torch.cat((samples, sample_i), dim=0)
             c = 0
-            # print(f"Accepted sample {samples.shape[0]}")
         else:
             c += 1
             if c > rejection_limit:
                 c = 0
                 d = d / 2
-            # print(f"d: {d}")
-    print(f"Object {i + 1} saved with class {data_y.item()}")
-    # torch.save(samples, f"sub_samples/object{i + 1}_class_{data_y.item()}.pt")
+    print(f"Object {i + 1} saved with class {data_y.item()}")")
     train_x.append(samples)
     train_y.append(data_y)
 train_x = torch.stack(train_x, dim=0)
@@ -57,13 +49,11 @@ test_y = []
 
 # Create testing set:
 for i in range(len(test_set)):
-    # print(f"Processing object {i + 1}...")
     data_y = test_set[i].y
     data_x_full = test_set[i].pos
     d = torch.tensor([1000.0])
     c = 0  # this counts number of successive rejections
     N = data_x_full.shape[0]
-    # print(f"N: {N}")
     samples = data_x_full[torch.randint(0, N, (1,))]  # create first data point
 
     while samples.shape[0] < new_size:
@@ -71,21 +61,15 @@ for i in range(len(test_set)):
         sample_i = data_x_full[idx]
         distances = torch.norm(samples - sample_i, dim=1)
         within_dist = (distances + 0.01 <= d).any()
-        # print(f"d: {d}, distances: {distances}")
         if not within_dist:
-            # print(f"samples shape: {samples.shape}")
-            # print(f"sample_i shape: {sample_i.shape}")
             samples = torch.cat((samples, sample_i), dim=0)
             c = 0
-            # print(f"Accepted sample {samples.shape[0]}")
         else:
             c += 1
             if c > rejection_limit:
                 c = 0
                 d = d / 2
-            # print(f"d: {d}")
     print(f"Object {i + 1} saved with class {data_y.item()}")
-    # torch.save(samples, f"sub_samples/object{i + 1}_class_{data_y.item()}.pt")
     test_x.append(samples)
     test_y.append(data_y)
 test_x = torch.stack(test_x, dim=0)
@@ -99,6 +83,3 @@ print(f"train_x shape: {train_x.shape}")
 print(f"train_y shape: {train_y.shape}")
 print(f"test_x shape: {test_x.shape}")
 print(f"test_y shape: {test_y.shape}")
-# print("Complete.")
-# train_x = torch.cat((train_x, samples), dim=0)
-# train_y = torch.cat((train_y, data_y))
