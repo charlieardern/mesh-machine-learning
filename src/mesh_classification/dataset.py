dea4bcc9495f1c561873818f770b4f63e6206c48
charlieardern <charlie.ardern@gmail.com>
2025-08-18 11:24:15 +0100

    Fixed incorrect property name in dataset

--- src/mesh_classification/dataset.py
+++ src/mesh_classification/dataset.py
@@ -21,15 +21,15 @@ class ModelNetSubset(Dataset):
             y = torch.load("data/ModelNet_subset/test_y.pt")
 
         # Selecting the categories of interest:
-        mask = torch.isin(y, torch.tensor(cats_of_interest)).squeeze()
+        mask = torch.isin(y, torch.tensor(cats)).squeeze()
         y = y[mask]
         x = x[mask]
 
         if norm_params_x is None:
             print(f"Shape with dim=1: {x.mean(dim=1).unsqueeze(1).shape}")
-            print(f"Shape with dim=(0,1): {x.mean(dim=(0,1)).shape}")
+            print(f"Shape with dim=(0,1): {x.mean(dim=(0, 1)).shape}")
             mean_x = x.mean(dim=1).unsqueeze(1)
-            std_x = x.std(dim=(1,2)).reshape(-1,1,1)
+            std_x = x.std(dim=(1, 2)).reshape(-1, 1, 1)
             print(f"std_x.shape: {std_x.shape}")
             self.norm_params_x = mean_x, std_x
             self.x = (x - mean_x) / std_x
@@ -38,8 +38,8 @@ class ModelNetSubset(Dataset):
             self.x = (x - norm_params_x[0]) / norm_params_x[1]
 
         # relabeling classes:
-        for i in range(len(cats_of_interest)):
-            mask = torch.isin(y, cats_of_interest[i])
+        for i in range(len(cats)):
+            mask = torch.isin(y, cats[i])
             y[mask] = i
         self.y = y
 
@@ -56,6 +56,4 @@ cats_of_interest = [0, 3, 5, 6, 7, 8, 9, 10, 15, 17, 19, 30, 35]
 # cats_of_interest = [0, 6, 8, 17, 35]
 
 train_dataset = ModelNetSubset(train=True, norm_params_x=None, cats=cats_of_interest)
-test_dataset = ModelNetSubset(
-    train=False, norm_params_x=None, cats=cats_of_interest
-)
+test_dataset = ModelNetSubset(train=False, norm_params_x=None, cats=cats_of_interest)
